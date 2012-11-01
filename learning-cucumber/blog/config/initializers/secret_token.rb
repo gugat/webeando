a87b920400efb6ec0c1410b0282b1954ae2bc1be
gustavo salazar <guga.salazar.loor@gmail.com>
2012-11-01 01:53:52 -0500

    Learning cucumber

---
+++ learning-cucumber/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '469ca51029010139e8275570be38e44af565e7bf4ca3569b6fce4662c49c7f56e7aaa06376d2520955985dbfa29f7efe0b8772e75fe93a5e0133b999b082d395'
